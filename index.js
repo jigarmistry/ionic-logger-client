2f0311311638ae18550718505bfb06a9eea04ac3
Jigar Mistry <jigarmistry24@gmail.com>
2017-09-26 09:40:26 +0530

    support for the config

--- index.js
+++ index.js
@@ -1,12 +1,44 @@
 var axios = require("axios");
+var ip = require("ip");
+var localForage = require("localforage");
 
-function callAPI(message, data) {
-  axios.post("http://localhost:4567/messages", {
+// Get the IP Address of the system
+var gHost = "localhost";
+var ipAddress = ip.address();
+if (ipAddress) {
+  gHost = ipAddress;
+}
+
+/*
+  Call the API to send the log
+*/
+function callAPI(message, data, host) {
+  axios.post(`http://${host}:4567/messages`, {
     message: message,
     data: data
   });
 }
 
+/*
+  Setup the config
+  host & devMode
+*/
+export function setupLog(data) {
+  localForage.setItem("config", JSON.stringify(data));
+}
+
+/*
+  Function to check for log
+*/
 export function logIt(message, data) {
-  callAPI(message, data);
+  localForage.getItem("config", function(err, value) {
+    if (value) {
+      var config = JSON.parse(value);
+      var host = config.host ? config.host : gHost;
+      var devMode = config.devMode ? config.devMode : "0";
+      if (devMode == "1") {
+        callAPI(message, data, host);
+      }
+    }
+  });
 }
